a949a32cf9c6e26ea33ea44c97e2cf8f560d5975
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-07-01 23:21:23 +0300

    Initial Commit

---
+++ MovieNightPlanner/MovieNightPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNightPlannerApp.swift
+//  MovieNightPlanner
+//
+//  Created by Salih Özdemir on 1.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieNightPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
