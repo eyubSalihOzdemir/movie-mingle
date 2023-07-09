bb48be51eb0bf02c23915da6bf99e1ddd07693a1
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-07-09 12:15:23 +0300

    this view model will be used for the services necessary for the profile screen

---
+++ MovieNightPlanner/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewModel.swift
+//  MovieNightPlanner
+//
+//  Created by Salih Özdemir on 9.07.2023.
+//
+
+import Foundation
+import FirebaseDatabase
+
+@MainActor class ProfileViewModel: ObservableObject {
+    //todo: Add the feature of being able to change the profile picture
+}
