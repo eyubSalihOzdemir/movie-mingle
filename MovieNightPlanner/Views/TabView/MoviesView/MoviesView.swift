a0409455c9fe30804df7ef6af7f122285af60d00
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-07-10 20:24:04 +0300

    added ability to know the scroll offset and scroll direction. these will be used to show and hide the custom navigation bar

--- MovieNightPlanner/Views/TabView/MoviesView/MoviesView.swift
+++ MovieNightPlanner/Views/TabView/MoviesView/MoviesView.swift
@@ -7,21 +7,59 @@
 
 import SwiftUI
 
+struct ScrollOffsetPreferenceKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
+    }
+}
+
 struct MoviesView: View {
+    @Namespace var scrollSpace
+    @State private var scrollOffset: CGFloat = 0
+    @State private var navigationBarHidden = false
+    
     var body: some View {
         ZStack(alignment: .top) {
             ScrollView {
-                Color.clear
-                    .frame(height: 60)
-                
-                ForEach(0..<20, id: \.self) { movie in
-                    RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 100, height: 100)
+                Group {
+                    Color.clear
+                        .frame(height: Constants.customNavBarHeight)
+                    
+                    ForEach(0..<20, id: \.self) { movie in
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 100, height: 100)
+                    }
+                    
+                    Color.clear
+                        .frame(height: Constants.customTabBarHeight)
+                }
+                .background(
+                    GeometryReader { geo in
+                        Color.clear
+                            .preference(key: ScrollOffsetPreferenceKey.self, value: geo.frame(in: .named(scrollSpace)).origin.y)
+                    }
+                )
+                .onPreferenceChange(ScrollOffsetPreferenceKey.self) { value in
+                    if value > scrollOffset {
+                        print("Scroll UP")
+                        //withAnimation {
+                        //    navigationBarHidden = false
+                        //}
+                    } else {
+                        print("Scroll DOWN")
+                        //withAnimation {
+                        //    navigationBarHidden = true
+                        //}
+                    }
+                    scrollOffset = value
                 }
             }
-            //.padding(.top, 60)
-            
-            CustomNavigationBar(title: "Movies") { }
+            .coordinateSpace(name: scrollSpace)
+
+            CustomNavigationBar(title: "\(scrollOffset)") { }
+                .offset(y: navigationBarHidden ? -100 : 0)
         }
     }
 }
