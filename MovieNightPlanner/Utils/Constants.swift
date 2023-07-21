447df089f7c2a441b2772f38693e5410f0c2ad13
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-07-21 15:30:23 +0300

    added genre ids to the constants file

--- MovieNightPlanner/Utils/Constants.swift
+++ MovieNightPlanner/Utils/Constants.swift
@@ -22,4 +22,49 @@ struct Constants {
     
     static let apiAuthenticationHeader = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTJlYmZlY2ZjOGUwNWIwNTAyOGI4OWNmMjI1Mzc1ZiIsInN1YiI6IjY0YWQzZDkxNmEzNDQ4MDBlYThlMGY5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ"
     static let apiKey = "112ebfecfc8e05b05028b89cf225375f"
+    
+    static func getGenre(id: Int) -> String {
+        switch id {
+        case 28:
+            return "Action"
+        case 12:
+            return "Adventure"
+        case 16:
+            return "Animation"
+        case 35:
+            return "Comedy"
+        case 80:
+            return "Crime"
+        case 99:
+            return "Documentary"
+        case 18:
+            return "Drama"
+        case 10751:
+            return "Family"
+        case 14:
+            return "Fantasy"
+        case 36:
+            return "History"
+        case 27:
+            return "Horror"
+        case 10402:
+            return "Music"
+        case 9648:
+            return "Mystery"
+        case 10749:
+            return "Romance"
+        case 878:
+            return "Sci-Fi"
+        case 10770:
+            return "TV Movie"
+        case 53:
+            return "Thriller"
+        case 10752:
+            return "War"
+        case 37:
+            return "Western"
+        default:
+            return "Unknown"
+        }
+    }
 }
