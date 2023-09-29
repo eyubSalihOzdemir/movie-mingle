9cb05a1fb1b4e7f69bc994f897e4af08bf60c0d1
eyubSalihOzdemir <eyupsalih776@gmail.com>
2023-09-29 14:55:05 +0300

    Added GoogleService-Info.plist

--- MovieNightPlanner/ViewModels/EventsViewModel.swift
+++ MovieNightPlanner/ViewModels/EventsViewModel.swift
@@ -85,7 +85,8 @@ import FirebaseDatabase
             }
         }
         
-        self.showingEventCreationSheet = false
+        self.showingEventCreationSheet.toggle()
+        self.resetValues()
     }
     
     func generateRandomHexColor() -> String {
@@ -98,5 +99,12 @@ import FirebaseDatabase
         let hexString = String(format: "#%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
         
         return hexString
+    }
+    
+    func resetValues() {
+        self.name = ""
+        self.place = ""
+        self.date = Date.now
+        self.people = [:]
     }
 }
